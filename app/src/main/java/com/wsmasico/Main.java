282bbe4d415decf66a59d3dfdfd5a0635b3f5183
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2024-04-16 02:32:06 -0500

    ajustndo dependencias

--- app/src/main/java/com/wsmasico/Main.java
+++ app/src/main/java/com/wsmasico/Main.java
@@ -15,83 +15,92 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
- import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
- 
 
+import com.wsmasico.Utils.Constantes;
+
+ 
+ 
 public class Main {
+
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         logger.info("Este es un mensaje de información");
 
-        // leyendo archivo excel 
-            File excelFile = new File("C:\\Users\\Miguel\\Desktop\\dataPuebaEnvioMaximoWhatsap.xlsx");
-            FileInputStream fis = new FileInputStream(excelFile);
-            XSSFWorkbook workbook = new XSSFWorkbook(fis);
-            XSSFSheet sheet = workbook.getSheetAt(0); 
+        File excelFile = new File("C:\\Users\\Miguel\\Desktop\\dataPuebaEnvioMaximoWhatsap.xlsx");
+        FileInputStream fis = new FileInputStream(excelFile);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheetAt(0);
 
-            List<String> numeros = new ArrayList<>();
-            for (Row row : sheet) {
-                Cell cell = row.getCell(0); // Suponiendo que los números están en la primera columna
-                numeros.add(cell.getStringCellValue());
-            }
-
-
-                //Leemos el chromedriver que esta en la misma direccion del programa
-                System.setProperty("webdriver.chrome.driver", "C:\\Users\\Miguel\\Downloads\\chromedriver-win64\\chromedriver.exe");
-                //Inicializamos ls opciones de chrome
-                ChromeOptions optionsGoo = new ChromeOptions();
-                //Permitimos la propiedad no-sandbox para evitar problemas en linux
-                optionsGoo.addArguments("--no-sandbox");
-                //Deshabilitamos las notificaciones
-                optionsGoo.addArguments("--disable-notifications");
-                //Guardamos la sesion en la carpeta chromeWA
-                optionsGoo.addArguments("--user-data-dir=C:\\Users\\Miguel\\Downloads\\chromedriver-win64\\data");
-                //Instanciamos un nuevo chromedriver
-                ChromeDriver  driver = new ChromeDriver(optionsGoo);
-            // Abriendo ws 
-            // System.setProperty("webdriver.chrome.driver", "C:\\Users\\Miguel\\Downloads\\chromedriver-win64\\chromedriver.exe");
-            // WebDriver driver = new ChromeDriver();
-            // driver.get("https://web.whatsapp.com");
-
-       // enviando mensjae de prueba 
-
-       for (String numero : numeros) {
-
-    String urlWhatsapp = "https://web.whatsapp.com/send/?phone=" + numero.trim()+"&text&type=phone_number&app_absent=0";
-    driver.get(urlWhatsapp);
-       //Declaramos el tiempo de espera
- 
-       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000L));
-     //Abrimos whatsapp web con el numero de telefono a enviar
-    // WebElement mensajeInput = driver.findElement(By.xpath("//div[@contenteditable='true']"));
-    // mensajeInput.sendKeys("Estoy probando envio masivo de ws");
-    
-    // WebElement enviarBtn = driver.findElement(By.xpath("//span[@data-icon='send']"));
-    // enviarBtn.click();
-
-    //Esperamos que cargue
-                    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")));
-                    //Escribimos el mensaje
-                    driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")).sendKeys("Holaaaaa soy un botsito");
-                    //Esperamos 1 segundo
-                    pausa(1000);
-                    //Precionamos enter
-                    driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")).sendKeys(Keys.ENTER);
-                    pausa(1000);
-}
-
-        System.out.println("Hello world!");
-    }
-
-    private static void pausa(int sleeptime) {
-        try {
-            Thread.sleep(sleeptime);
-        } catch (InterruptedException ex) {
+        List<String> numeros = new ArrayList<>();
+        for (Row row : sheet) {
+            Cell cell = row.getCell(0); // Suponiendo que los números están en la primera columna
+            numeros.add(cell.getStringCellValue());
         }
+        workbook.close();
+
+        logger.info(numeros.toString());
+
+
+
+ // Configura y descarga automáticamente el ChromeDriver correspondiente
+ //WebDriverManager.chromedriver().driverVersion("123.0.6312.122").setup();
+ System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Miguel\\Desktop\\wsMasivo\\app\\chromedriver.exe");
+         // Inicializamos ls opciones de chrome
+         ChromeOptions optionsGoo = new ChromeOptions();
+         // Permitimos la propiedad no-sandbox para evitar problemas en linux
+         optionsGoo.addArguments("--no-sandbox");
+         // Deshabilitamos las notificaciones
+         optionsGoo.addArguments("--disable-notifications");
+         // Guardamos la sesion en la carpeta chromeWA
+         optionsGoo.addArguments("--user-data-dir=C:\\Users\\Miguel\\Desktop\\wsMasivo\\app\\data");
+ 
+ // Crea una instancia de ChromeDriver   
+ WebDriver driver = new ChromeDriver(optionsGoo);
+
+ // Abre una página, por ejemplo, Google
+ driver.get("https://www.google.com");
+
+ // Aquí puedes añadir más acciones para interactuar con la página
+
+ // Cierra el navegador
+
+
+
+
+
+        
+        
+  
+
+        for (String numero : numeros) {
+
+            String urlWhatsapp = "https://web.whatsapp.com/send/?phone=" + numero.trim();
+            driver.get(urlWhatsapp);
+            // Declaramos el tiempo de espera
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3000L));
+ 
+            // Esperamos que cargue
+            wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")));
+            // Escribimos el mensaje
+            driver.findElement(By.xpath(Constantes.RUTA_BOX_MSJ)).sendKeys("MENSAJE TEST");
+            // Esperamos 1 segundo
+            Thread.sleep(1000);
+            // Precionamos enter
+            driver.findElement(By.xpath(Constantes.RUTA_BOX_BTNENVIAR)).sendKeys(Keys.ENTER);
+            Thread.sleep(2000);
+        }
+        driver.quit();
     }
+
+    
 }
---
+++ app/src/main/java/com/wsmasico/Utils/Constantes.java
@@ -0,0 +1,7 @@
+package com.wsmasico.Utils;
+
+public class Constantes {
+    public static final String RUTA_BOX_MSJ = "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p";
+    public static final String RUTA_BOX_BTNENVIAR = "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p";
+
+}
